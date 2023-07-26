55fd81567f504615442b86ed071341140d73e768
Minhanh245 <140375817+Minhanh245@users.noreply.github.com>
2023-07-26 14:10:21 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+            strings: ["Aesthestic.", "Women's clothing.","Accessory."],
+       	 	typeSpeed:30,
+            backSpeed:30,
+            loop:true,
+     });
+$('.counter').countUp();
+
---
+++ js/tabcontent2.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+function openCity2(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks2");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen2" and click on it
+document.getElementById("defaultOpen2").click();
